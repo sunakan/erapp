a497230bc075b7b549f8dbf3d86510855178ceaa
sunakan <sunakan03@gmail.com>
2019-12-06 22:20:03 +0900

    [Add] TerraformでIAMグループと作成したポリシーのアタッチ

--- terraform/iam/main.tf
+++ terraform/iam/main.tf
@@ -1,6 +1,15 @@
-resource "aws_iam_policy" "deploy" {
-  name = "ecr-deploy"
-  path = "/"
+resource "aws_iam_policy" "deploy_policy" {
+  name        = "ecr-deploy"
+  path        = "/"
   description = "deploy to ecr"
-  policy = file("iam/policies/ecr_push.json")
+  policy      = file("iam/policies/ecr_push.json")
+}
+
+resource "aws_iam_group" "deploy_group" {
+  name = "deploy_ecr"
+}
+
+resource "aws_iam_group_policy_attachment" "attach_deploy" {
+  group      = aws_iam_group.deploy_group.name
+  policy_arn = aws_iam_policy.deploy_policy.arn
 }
