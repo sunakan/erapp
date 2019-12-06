4a749e04339893b0578be29eb8df6deb479cab26
sunakan <sunakan03@gmail.com>
2019-12-06 22:31:10 +0900

    [Add] 作ったグループに所属したIAMユーザを作成

--- terraform/iam/main.tf
+++ terraform/iam/main.tf
@@ -13,3 +13,13 @@ resource "aws_iam_group_policy_attachment" "attach_deploy" {
   group      = aws_iam_group.deploy_group.name
   policy_arn = aws_iam_policy.deploy_policy.arn
 }
+
+resource "aws_iam_user" "deploy_user" {
+  name = "deploy-user"
+}
+
+resource "aws_iam_group_membership" "deploy_membership" {
+  name  = "deploy-membership"
+  users = [aws_iam_user.deploy_user.name]
+  group = aws_iam_group.deploy_group.name
+}
