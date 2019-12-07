0c16f2793e70f4ba128e756a7d5e0e3fa4475b40
sunakan <sunakan03@gmail.com>
2019-12-07 11:45:15 +0900

    [Add] 手元でECRへDockerImageをプッシュできるようにする

--- Makefile
+++ Makefile
@@ -1,11 +1,19 @@
 DOCKER_IMAGE=sunakan/erapp
 RUBY_VERSION_TAG=2.6-slim
+AWS_ECR_REPO_NAME=sunakan/erapp
 
 export DOCKER_IMAGE
 export RUBY_VERSION_TAG
+export AWS_ECR_REPO_NAME
+
+login:
+	aws ecr get-login --no-include-email | sh
 
 build:
-	DOCKER_IMAGE=${DOCKER_IMAGE} bash ./build-deploy/build.sh
+	bash ./build-deploy/build.sh
+
+deploy: login
+	bash ./build-deploy/build.sh | tail -1 | bash ./build-deploy/push-to-ecr.sh
 
 run:
 	docker run --publish 4567:4567 ${DOCKER_IMAGE}:latest
---
+++ build-deploy/push-to-ecr.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# -u オプションは未定義変数を使うとError
+set -u
+
+# DOCKER_IMAGE_WITH_TAGが未定義なら、標準入力で読み込む
+if [ -z "${DOCKER_IMAGE_WITH_TAG:+DOCKER_IMAGE_WITH_TAG}" ]; then
+  read DOCKER_IMAGE_WITH_TAG
+fi
+
+# AWS_ECR_REGISTRYが未定義なら、ACCOUNT_IDを取得して作る(手動用)
+if [ -z "${AWS_ECR_REGISTRY:+$AWS_ECR_REGISTRY}" ]; then
+  AWS_ACCOUNT_ID=`aws sts get-caller-identity | jq -r '.Account'`
+  AWS_REGION=ap-northeast-1
+  AWS_ECR_REGISTRY="${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com"
+fi
+
+# ECRにpushする用のimage tagを付与
+docker tag ${DOCKER_IMAGE_WITH_TAG} ${AWS_ECR_REGISTRY}/${AWS_ECR_REPO_NAME}:${DOCKER_IMAGE_WITH_TAG##*:}
+docker push ${AWS_ECR_REGISTRY}/${AWS_ECR_REPO_NAME}:${DOCKER_IMAGE_WITH_TAG##*:}
