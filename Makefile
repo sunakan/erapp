8ec8a44d825ed5fa2fbcdfa207eec6bb55559162
sunakan <sunakan03@gmail.com>
2019-12-07 11:27:08 +0900

    [Add] DockerImageを作成する工程をスクリプトにする
    - GitHub Actionsで利用するため

--- Makefile
+++ Makefile
@@ -1,11 +1,14 @@
+DOCKER_IMAGE=sunakan/erapp
+RUBY_VERSION_TAG=2.6-slim
+
+export DOCKER_IMAGE
+export RUBY_VERSION_TAG
+
 build:
-	docker build \
-	--tag sunakan/erapp:latest \
-	--build-arg RUBY_VERSION_TAG=2.6-slim \
-	.
+	DOCKER_IMAGE=${DOCKER_IMAGE} bash ./build-deploy/build.sh
 
 run:
-	docker run --publish 4567:4567 sunakan/erapp:latest
+	docker run --publish 4567:4567 ${DOCKER_IMAGE}:latest
 
 up:
 	docker-compose up
---
+++ build-deploy/build.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# 実行例
+# echo "${HOGE}" | bash ./build.sh
+# or
+# DOCKER_IMAGE="${HOGE}" bash ./build.sh
+
+# -u オプションは未定義変数を使うとError
+set -u
+
+# ${hoge##*/}でブランチ名が/で区切られていたとき、最後だけ持ってくる
+# 強み：feature/hoge
+# 弱み：alpha/hogeとbeta/hoge
+# TODO: GithubActionsでもここでブランチ名を取得できるなら、if文消せる
+if [ -z "${CURRENT_BRANCH:+$CURRENT_BRANCH}" ]; then
+  CURRENT_BRANCH=`git rev-parse --abbrev-ref HEAD`
+fi
+CURRENT_BRANCH=${CURRENT_BRANCH##*/}
+
+# コミットIDの最初の8文字
+COMMIT_ID=`git show -s --format=%H`
+COMMIT_ID=${COMMIT_ID:0:8}
+
+# DockerImageのビルド
+if [ -z "${DOCKER_IMAGE:+$DOCKER_IMAGE}" ]; then
+  read DOCKER_IMAGE
+fi
+NGINX_VERSION_TAG=stable
+IMAGE_TAG="${CURRENT_BRANCH}--${COMMIT_ID}"
+
+docker build \
+  --tag ${DOCKER_IMAGE}:${IMAGE_TAG} \
+  --build-arg RUBY_VERSION_TAG=${RUBY_VERSION_TAG} \
+  .
+
+docker tag ${DOCKER_IMAGE}:${IMAGE_TAG} ${DOCKER_IMAGE}:latest
+
+# 以下のように書かない理由：GitHubActionと手動実行時とできるだけ同じにするため
+# echo "##[set-output name=image_tag;]$(echo ${IMAGE_TAG})"
+echo "built docker image"
+echo ${DOCKER_IMAGE}:${IMAGE_TAG}
