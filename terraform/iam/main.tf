d601e099ede3f343b5ded442146eede1bce2b2f1
sunakan <sunakan03@gmail.com>
2019-12-07 16:33:22 +0900

    [Add] ECSインスタンス用のroleにECSインスタンス用のポリシーをアタッチ

--- terraform/iam/main.tf
+++ terraform/iam/main.tf
@@ -41,3 +41,8 @@ resource "aws_iam_policy" "ecs_instance_policy" {
   description = ""
   policy = file("iam/policies/ecs_instance.json")
 }
+
+resource "aws_iam_role_policy_attachment" "ecs_instance_role_attach" {
+  role       = aws_iam_role.ecs_instance_role.name
+  policy_arn = aws_iam_policy.ecs_instance_policy.arn
+}
